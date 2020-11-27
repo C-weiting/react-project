f29243105d21bd56dbb98de7d3ff98ca558bce55
chenweiting <weiting.chen@argrace.ai>
2020-11-27 17:27:06 +0800

    commit

--- src/views/Message/index.js
+++ src/views/Message/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navigation from '@/components/Navigation';
 import Empty from '@/components/Empty';
-import showMessageModel from '@/components/MessageModel';
 import useMessageList from '@/hooks/useMessageList';
 import useMessageSub from '@/hooks/useMessageSub';
+import messageClick from '@/utils/messageClick';
 import { showTime } from '@/utils';
-import { CustomFail } from '@/components/CustomToast';
-import { getPropertyBlockInformationPicDetail, queryPropertyOrderDetail } from '@/api/message';
 import './message.less';
 
 function Message () {
@@ -15,48 +13,7 @@ function Message () {
     // handleClick({type: 10092, id1: 6491})
 
     function handleClick (item) {
-        if (parseInt(item.type) === 10092) { // 已缴费通知
-            const orderId = item.id1; // 12011181533601682
-            let params = {
-                orderId: orderId,
-                data: `orderId=${orderId}`
-            }
-            queryPropertyOrderDetail(params).then(res => {
-                if (res.success && res.model) {
-                    let { propertyBillList, orderId, paymentTime, paymentTypeName, paymentChannelName } = res.model
-                    if (propertyBillList && propertyBillList.length) {
-                        const { subBillMonth, subFeeName } = propertyBillList[0];
-
-                        let messageDetail = {
-                            title: '已缴费通知',
-                            createTime: showTime(item.createTime),
-                            content: `您已缴纳${subFeeName}，缴纳账期${subBillMonth}，订单编号{${orderId}}，支付时间{${paymentTime}}，支付方式{${paymentTypeName}}，缴纳渠道{${paymentChannelName}}`
-                        }
-                        showMessageModel(messageDetail);
-                    }
-                } else {
-                    CustomFail('请求详情失败');
-                }
-            });
-        } else {
-            let params = {
-                informationId: item.id1,
-                sourceType: 1
-            }
-            getPropertyBlockInformationPicDetail(params).then(res => {
-                if (res.success && res.model && res.model.informationNote) {
-                    const { note } = res.model.informationNote
-                    let messageDetail = {
-                        title: parseInt(item.type) === 10090 ? '社区公告通知' : '欠费通知',
-                        createTime: showTime(item.createTime),
-                        content: note
-                    }
-                    showMessageModel(messageDetail);
-                } else {
-                    CustomFail('请求详情失败');
-                }
-            });
-        }
+        messageClick(item);
     }
 
     return (
